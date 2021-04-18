7f7967a1324a9b439612c69c1afc8d53704b2aa3
PWidmann <philipwidmann1@gmail.com>
2021-04-18 23:50:59 +0200

    Initial project commit with simple player and camera controlle

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform cameraTarget;
+    [SerializeField] float smoothTime = 0.5f;
+
+    private Vector3 velocity;
+    private Vector3 correctedCamTarget;
+
+    void LateUpdate()
+    {
+        correctedCamTarget = cameraTarget.position;
+        correctedCamTarget.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, correctedCamTarget, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1.3f;
+
+    CharacterController controller;
+    Animator animator;
+
+    // Input
+    Vector2 input;
+    float horizontal;
+    float vertical;
+    Vector2 movement;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        input = new Vector2(horizontal, vertical).normalized;
+        
+        if (input != Vector2.zero)
+        {
+            animator.SetFloat("movementX", horizontal);
+            animator.SetFloat("movementY", vertical);
+            animator.SetBool("isMoving", true);
+
+            movement = input * moveSpeed * Time.deltaTime;
+            controller.Move(movement);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
